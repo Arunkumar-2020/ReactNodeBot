8787393d5aa7910acdc3dc5dbcf8d822400d6883
Arunkumar-2020 <arunkumar.ak.k.2020@gmail.com>
2021-09-11 11:57:32 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+// root handler for landing page
+app.get('/',(req, res) => {
+    res.send({'hello':'there'});
+
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(5000);
+
