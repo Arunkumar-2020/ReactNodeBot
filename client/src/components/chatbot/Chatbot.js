8ad88cc9942bc15daf81c1fefdbb03de05e3a811
Arunkumar-2020 <arunkumar.ak.k.2020@gmail.com>
2021-10-28 17:00:01 +0530

    api calls to backend and simple message component added

--- client/src/components/chatbot/Chatbot.js
+++ client/src/components/chatbot/Chatbot.js
@@ -1,4 +1,7 @@
 import React,{ Component } from 'react';
+import axios from 'axios/index';
+
+import Message from './Message';
 
 class Chatbot extends Component
 {
@@ -9,12 +12,62 @@ class Chatbot extends Component
             messages: []
         }
     }
+
+    async df_text_query(text){
+        let says = {
+            speaks: 'me',
+            msg: {
+                text:{
+                    text:text
+                }
+            }
+        };
+
+        this.setState({messages: [...this.messages,says]});
+        const res = await axios.post('/api/df_text_query',{text});
+        
+        for(let msg of res.data.fullfillmentMessages){
+            let says = {
+                speaks: 'bot',
+                msg: msg
+            };
+
+            this.setState({messages: [...this.messages,says]});
+        }
+
+    }
+
+    async df_event_query(event){
+        const res = await axios.post('/api/df_event_query',{event});
+
+        for(let msg of res.data.fullfillmentMessages){
+            let says = {
+                speaks: 'bot',
+                msg: msg
+            }
+            this.setState({messages: [...this.messages,says]});
+        }
+    }
+
+    renderMessages(stateMessages){
+        if(stateMessages)
+        {
+            return stateMessages.map((message,i)=>{
+                return <Message key={i} speaks = {message.speaks} text = {message.text.text} />;
+                });
+        }
+        else{
+            return null;
+        }
+    }
+
     render()
     {
         return(
         <div style ={{height:400,width:400,float:'right'}}>
             <div id="chatbot" style={{height:'100%',width:'100%',overflow:'auto'}}>
                 <h2>Chatbot</h2>
+                {this.renderMessages(this.state.messages)}
                 <input type="text"/>
             </div>
         </div>
