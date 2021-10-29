2bd02b0b449e1c35dbc0e7c2c571dcc52b475d95
Arunkumar-2020 <arunkumar.ak.k.2020@gmail.com>
2021-10-29 22:57:29 +0530

    show and hide chatbot feature added

--- client/src/components/chatbot/Chatbot.js
+++ client/src/components/chatbot/Chatbot.js
@@ -12,15 +12,19 @@ const cookies = new Cookies();
 
 class Chatbot extends Component{
     messagesEnd;
-    //talkInput;
+    talkInput;
 
     constructor(props){
         super(props);
 
         this._handleInputKeyPress = this._handleInputKeyPress.bind(this);
         this._handleQuickReplyPayload = this._handleQuickReplyPayload.bind(this);
+        
+        this.hide = this.hide.bind(this);
+        this.show = this.show.bind(this); 
         this.state={
-            messages: []
+            messages: [],
+            showBot: true,
         };
         if (cookies.get('userID') === undefined) {
             cookies.set('userID', uuid(), { path: '/' });
@@ -71,7 +75,21 @@ class Chatbot extends Component{
     componentDidUpdate() {
         //this.talkInput.focus();
         this.messagesEnd.scrollIntoView({behaviour:"smooth"});
+        
+        
     }
+    show(event) {
+        event.preventDefault();
+        event.stopPropagation();
+        this.setState({showBot: true});
+    }
+
+    hide(event) {
+        event.preventDefault();
+        event.stopPropagation();
+        this.setState({showBot: false});
+    }
+
     _handleQuickReplyPayload(event, payload, text) {
         event.preventDefault();
         event.stopPropagation();
@@ -79,6 +97,7 @@ class Chatbot extends Component{
         switch (payload) {
             case 'training_masterclass':
                 this.df_event_query('MASTERCLASS');
+                // eslint-disable-next-line
             default:
                 this.df_text_query(text);
                 break;
@@ -145,27 +164,49 @@ class Chatbot extends Component{
 
     render()
     {
-        return(
-            <div style={{ minHeight: 500, maxHeight: 500, width:400, position: 'absolute', bottom: 0, right: 0, border: '1px solid lightgray'}}>
-            <nav>
-                <div className="nav-wrapper">
-                    <a href="/" className="brand-logo">ChatBot</a>
-                </div>
-            </nav>
+       
+        if (this.state.showBot) {
+            return (
+                <div style={{ minHeight: 500, maxHeight: 500, width:400, position: 'absolute', bottom: 0, right: 0, border: '1px solid lightgray'}}>
+                    <nav>
+                        <div className="nav-wrapper">
+                            <a href="/" className="brand-logo">ChatBot</a>
+                            <ul id="nav-mobile" className="right hide-on-med-and-down">
+                                <li><a href="/" onClick={this.hide}>Close</a></li>
+                            </ul>
+                        </div>
+                    </nav>
 
-            <div id="chatbot"  style={{ minHeight: 388, maxHeight: 388, width:'100%', overflow: 'auto'}}>
+                <div id="chatbot"  style={{ minHeight: 388, maxHeight: 388, width:'100%', overflow: 'auto'}}>
 
                 {this.renderMessages(this.state.messages)}
-                <div ref ={(el) => { this.messagesEnd = el;}} style = {{float:'left',clear:'both'}}>
+                <div ref={(el) => { this.messagesEnd = el; }}
+                             style={{ float:"left", clear: "both" }}>
+                        </div>
+                    </div>
+                    <div className=" col s12" >
+                        <input style={{margin: 0, paddingLeft: '1%', paddingRight: '1%', width: '98%'}} ref={(input) => { this.talkInput = input; }} placeholder="type a message:"  onKeyPress={this._handleInputKeyPress} id="user_says" type="text" />
+                    </div>
+
                 </div>
-               {/* <input type="text" onKeyPress={this._handleInputKeyPress}/> */}
-               <div className=" col s12" >
-                    <input style={{margin: 0, paddingLeft: '1%', paddingRight: '1%', width: '98%'}} ref={(input) => { this.talkInput = input; }} placeholder="type a message:"  onKeyPress={this._handleInputKeyPress} id="user_says" type="text" />
+            );
+        } else {
+            return (
+                <div style={{ minHeight: 40, maxHeight: 500, width:400, position: 'absolute', bottom: 0, right: 0, border: '1px solid lightgray'}}>
+                    <nav>
+                        <div className="nav-wrapper">
+                            <a href="/" className="brand-logo">ChatBot</a>
+                            <ul id="nav-mobile" className="right hide-on-med-and-down">
+                                <li><a href="/" onClick={this.show}>Show</a></li>
+                            </ul>
+                        </div>
+                    </nav>
+                    <div ref={(el) => { this.messagesEnd = el; }}
+                         style={{ float:"left", clear: "both" }}>
+                    </div>
                 </div>
-            </div>
-            </div>
-        
-        );
+            );
+        }
     } 
 }
 
